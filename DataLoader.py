509fb8fa2d033f71d9e3972c81c3aadea9e6788d
Daniel Sanche <d.sanche14@gmail.com>
2016-10-31 01:04:55 -0600

    added more to csv file

--- DataLoader.py
+++ DataLoader.py
@@ -1,6 +1,8 @@
 import os
 from scipy.io import loadmat
 import pandas as pd
+from datetime import datetime, timedelta
+import numpy as np
 
 datasetDir = "/Users/Sanche/Datasets/IMDB-WIKI"
 #for root, dirs, files in os.walk(datasetDir):
@@ -18,12 +20,29 @@ faceLocation = matFile["wiki"]["face_location"][0][0][0]
 faceScore =  matFile["wiki"]["face_score"][0][0][0]
 faceScore2 =  matFile["wiki"]["second_face_score"][0][0][0]
 
-age = dateOfBirth - yearTaken
+birthYear = np.zeros(dateOfBirth.shape)
+age = np.zeros(dateOfBirth.shape)
 
-dataTable = {"name":name, "birthday":dateOfBirth, "year_taken":yearTaken, "isMale":gender,
+for i in range(0, dateOfBirth.shape[0]):
+    #add age/birth year
+    matlabBD = dateOfBirth[i]
+    pythonBd = datetime.fromordinal(int(matlabBD)) + timedelta(days=int(matlabBD) % 1) - timedelta(days=366)
+    birthYear[i] = pythonBd.year
+    age[i] = yearTaken[i] - pythonBd.year
+    #fix name
+    nameArr = name[i]
+    if(nameArr.shape[0] >0):
+        name[i] = nameArr[0].replace(",", "")
+    else:
+        name[i] = ""
+    #fix path
+    pathArr = path[i]
+    path[i] = pathArr[0]
+
+
+dataTable = {"name":name, "age":age, "birthday":birthYear, "year_taken":yearTaken, "isMale":gender,
              "face_location":faceLocation, "face_score":faceScore, "second_face":faceScore2, "path":path}
 
 df = pd.DataFrame(dataTable)
-df.to_csv("./wiki.csv")
+df.to_csv("./wiki.csv", index=False)
 
-print (df)

15ca901d3583bef8ac8190bb834843608639c114
Daniel Sanche <d.sanche14@gmail.com>
2016-11-01 17:08:37 -0600

    added function to display batch as a giant image

--- Visualization.py
+++ Visualization.py
@@ -1,7 +1,46 @@
 import sys
 import os
 import pandas as pd
+import  numpy as np
 import pickle
+from DataLoader import getBatch
+from math import ceil, sqrt
+from scipy.misc import  imsave, imresize
+
+def visualizeBatch(batchOutput, fileName="batch.png", maxSize=[10000, 10000, 3]):
+    imageVec = batchOutput["image"]
+    sexVec = batchOutput["sex"]
+    ageVec = batchOutput["age"]
+
+    imageSize = [imageVec.shape[1], imageVec.shape[2], imageVec.shape[3]]
+    numItems = sexVec.shape[0]
+    sqrtItems = sqrt(numItems)
+    numRows = int(ceil(sqrtItems))
+    numCols = numRows
+
+    CombinedImage = np.ones([imageSize[0]*numRows, imageSize[1]*numCols, imageSize[2]])
+    i = 0
+    rowStart = 0
+    for r in range(numRows):
+        rowEnd = rowStart + imageSize[0]
+        RowImage = np.zeros([imageSize[0], imageSize[1]*numCols, imageSize[2]])
+        lastStart = 0
+        for c in range(numCols):
+            thisImage = imageVec[i,:,:]
+            thisSex = sexVec[i]
+            thisAge = ageVec[i]
+            end = lastStart + imageSize[1]
+            RowImage[:, lastStart:end, :] = thisImage
+            lastStart = end
+            i = i + 1
+            if i >= numItems:
+                break
+        CombinedImage[rowStart:rowEnd,:,:] = RowImage
+        rowStart = rowEnd
+    if maxSize is not None:
+        CombinedImage = imresize(CombinedImage, imageSize)
+    imsave(fileName, CombinedImage)
+
 
 
 if __name__ == "__main__":
@@ -16,10 +55,14 @@ if __name__ == "__main__":
         print("one or both files not found")
         exit()
 
-print("restoring csv data...")
-csvdata = pd.read_csv(csvPath)
+    print("restoring csv data...")
+    csvdata = pd.read_csv(csvPath)
 
-print("restoring indices data...")
-file = open(indicesPath, "rb")
-indices = pickle.load(file)
-file.close()
+    print("restoring indices data...")
+    file = open(indicesPath, "rb")
+    indices = pickle.load(file)
+    file.close()
+
+    state = None
+    batchData, state, didFinish = getBatch(indices, csvdata, prevState=state)
+    visualizeBatch(batchData)

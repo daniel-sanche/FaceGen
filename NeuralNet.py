4f58d24d69c76f0899ff856a642f267f2788515e
Daniel Sanche <d.sanche14@gmail.com>
2016-11-10 03:23:20 -0600

    you now can tell the network whether to train the discriminator or the generator

--- NeuralNet.py
+++ NeuralNet.py
@@ -5,25 +5,31 @@ import numpy as np
 from nnLayers import create_fully_connected_layer, create_max_pool_layer, create_deconv_layer, \
     create_conv_layer, create_output_layer, create_unpool_layer
 from Visualization import visualizeImages
+from enum import Enum
 
+class NetworkType(Enum):
+    Generator=0,
+    Discriminator=1
 
 class NeuralNet(object):
-    def __init__(self, batch_size=1000, image_size=64, noise_size=20, age_range=[10, 100]):
+    def __init__(self, trainingType, batch_size=1000, image_size=64, noise_size=20, age_range=[10, 100]):
+        self.trainingType = trainingType
         self.age_range = age_range
         self.batch_size = batch_size
         self.image_size = image_size
         self.noise_size=noise_size
 
         self.dropout =  tf.placeholder(tf.float32)
-        self._buildGenerator(fcSize=64)
-        self._buildDiscriminator(conv1Size=32, conv2Size=64, fcSize=49)
+        trainGen = (trainingType==NetworkType.Generator)
+        self._buildGenerator(fcSize=64, train=trainGen)
+        self._buildDiscriminator(conv1Size=32, conv2Size=64, fcSize=49, train=~trainGen)
         self._buildCostFunctions()
 
         sess = tf.Session()
         sess.run(tf.initialize_all_variables())
         self.session = sess
 
-    def _buildGenerator(self, fcSize):
+    def _buildGenerator(self, fcSize, train=True):
         sqrtFc = int(sqrt(fcSize))
 
         # build the generator network
@@ -33,22 +39,22 @@ class NeuralNet(object):
         gen_input_combined = tf.concat(1, [gen_input_age, gen_input_gender, gen_input_noise])
         gen_fully_connected1, var_dict = create_fully_connected_layer(gen_input_combined, fcSize,
                                                                       self.noise_size + 2,
-                                                                      self.dropout, trainable=True, name_prefix="gen_fc")
+                                                                      self.dropout, trainable=train, name_prefix="gen_fc")
         gen_squared_fc1 = tf.reshape(gen_fully_connected1, [self.batch_size, sqrtFc, sqrtFc, 1])
         # now [1000,8,8,1]
         gen_unpool1 = create_unpool_layer(gen_squared_fc1)
         # now [1000,16,16,1]
-        gen_unconv1, var_dict = create_deconv_layer(gen_unpool1, 5, 1, trainable=True, name_prefix="gen_unconv1",
+        gen_unconv1, var_dict = create_deconv_layer(gen_unpool1, 5, 1, trainable=train, name_prefix="gen_unconv1",
                                                     var_dict=var_dict)
         # now [1000,16,16,5]
         gen_unpool2 = create_unpool_layer(gen_unconv1)
         # now [1000,32,32,5]
-        gen_unconv2, var_dict = create_deconv_layer(gen_unpool2, 5, 5, trainable=True, name_prefix="gen_unconv2",
+        gen_unconv2, var_dict = create_deconv_layer(gen_unpool2, 5, 5, trainable=train, name_prefix="gen_unconv2",
                                                     var_dict=var_dict)
         # now [1000,32,32,5]
         gen_unpool3 = create_unpool_layer(gen_unconv2)
         # now [1000,64,64,5]
-        gen_unconv3, var_dict = create_deconv_layer(gen_unpool3, 3, 5, trainable=True, name_prefix="gen_unconv3",
+        gen_unconv3, var_dict = create_deconv_layer(gen_unpool3, 3, 5, trainable=train, name_prefix="gen_unconv3",
                                                     var_dict=var_dict)
         # now [1000,64,64,5]
         totalPixels = self.image_size * self.image_size * 3
@@ -62,7 +68,7 @@ class NeuralNet(object):
         self.gen_input_gender = gen_input_gender
         self.vardict = var_dict
 
-    def _buildDiscriminator(self, conv1Size, conv2Size, fcSize):
+    def _buildDiscriminator(self, conv1Size, conv2Size, fcSize, train=True):
         num_pixels = self.image_size * self.image_size * 3
         dis_input_image = tf.placeholder(tf.float32, shape=[self.batch_size, num_pixels])
         dis_input_age = tf.placeholder(tf.float32, shape=[self.batch_size, 1])
@@ -76,12 +82,12 @@ class NeuralNet(object):
         # [2000, 12288]
         dis_reshaped_inputs = tf.reshape(dis_combined_inputs, [self.batch_size * 2, self.image_size, self.image_size, 3])
         # [2000, 64, 64, 3]
-        dis_conv1, var_dict = create_conv_layer(dis_reshaped_inputs, conv1Size, 3, trainable=True,
+        dis_conv1, var_dict = create_conv_layer(dis_reshaped_inputs, conv1Size, 3, trainable=train,
                                                 name_prefix="dis_conv1", var_dict=self.vardict)
         # [2000, 64, 64, 32]
         dis_pool1 = create_max_pool_layer(dis_conv1)
         # [2000, 32, 32, 32]
-        dis_conv2, var_dict = create_conv_layer(dis_pool1, conv2Size, conv1Size, trainable=True,
+        dis_conv2, var_dict = create_conv_layer(dis_pool1, conv2Size, conv1Size, trainable=train,
                                                 name_prefix="dis_conv2", var_dict=var_dict)
         # [2000, 32, 32, 64]
         dis_pool2 = create_max_pool_layer(dis_conv2)
@@ -90,13 +96,14 @@ class NeuralNet(object):
         # [2000, 16384]
         dis_fully_connected1, var_dict = create_fully_connected_layer(dis_pool2_flattened, fcSize,
                                                                       16 * 16 * conv2Size, self.dropout,
-                                                                      trainable=True,
+                                                                      trainable=train,
                                                                       name_prefix="dis_fc", var_dict=var_dict)
         # [2000, 49]
         dis_output_layer, var_dict = create_output_layer(dis_fully_connected1, fcSize, 3,
-                                                         trainable=True, name_prefix="dis_out",
+                                                         trainable=train, name_prefix="dis_out",
                                                          var_dict=var_dict)
         # [2000, 3]
+
         # save important nodes
         self.dis_input_image = dis_input_image
         self.dis_input_age = dis_input_age
@@ -160,8 +167,12 @@ class NeuralNet(object):
         self.gen_accuracy_age = genAgeAccuracy
         self.gen_accuracy_sex = genSexAccuracy
 
-
     def train(self, truthImages, truthGenders, truthAges, print_results=False):
+        if self.trainingType == NetworkType.Discriminator:
+            self._trainDiscriminator(truthImages=truthImages, truthGenders=truthGenders, truthAges=truthAges, print_results=print_results)
+
+
+    def _trainDiscriminator(self, truthImages, truthGenders, truthAges, print_results):
         batch_size = self.batch_size
         noise_batch = np.random.random_sample((batch_size, self.noise_size))
         ageVec = (np.linspace(start=self.age_range[0],stop=self.age_range[1],num=batch_size)+np.random.sample(batch_size))
@@ -174,11 +185,11 @@ class NeuralNet(object):
             feed_dict[self.dropout] = 1
             outputList = (self.dis_cost,self.dis_accuracy_age,self.dis_accuracy_sex,self.dis_accuracy_truth)
             cost, ageAcc,sexAcc, truthAcc = self.session.run(outputList, feed_dict=feed_dict)
-            print("cost: ", cost, " Accuracies: (", truthAcc,ageAcc,sexAcc,")")
+            print("cost: ", cost, " Accuracies: (t:", truthAcc,"a:",ageAcc,"s:",sexAcc,")")
         feed_dict[self.dropout] = 0.5
         self.session.run(self.dis_train, feed_dict=feed_dict)
 
-            #generatedImages = self.session.run(self.gen_output, feed_dict=feed_dict)
+        #generatedImages = self.session.run(self.gen_output, feed_dict=feed_dict)
         #generatedImages = np.reshape(generatedImages, [batch_size, self.image_size, self.image_size, 3])
         #visualizeImages(generatedImages[:50, :, :, :], numRows=5)
 
@@ -202,6 +213,6 @@ batchSex = batchDict["sex"]
 batchImage = batchImage.reshape([batch_size, -1])
 
 #start training
-network = NeuralNet(batch_size=batch_size, image_size=image_size, noise_size=20)
-network.train(batchImage, batchSex, batchAge, print_results=True)
+discriminator = NeuralNet(trainingType=NetworkType.Discriminator, batch_size=batch_size, image_size=image_size, noise_size=20)
+discriminator.train(batchImage, batchSex, batchAge, print_results=True)
 print("done")

98f4a8a26ec2194401dfba8e8df7e5b81cd8beb6
Daniel Sanche <d.sanche14@gmail.com>
2016-11-09 19:58:33 -0600

    added age and gender to generator inputs

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,10 +1,11 @@
 from DataLoader import DataLoader, LoadFilesData
 import tensorflow as tf
-from math import sqrt
+from math import sqrt, ceil
 import numpy as np
 from nnLayers import create_fully_connected_layer, create_max_pool_layer, create_deconv_layer, create_conv_layer, create_output_layer, create_unpool_layer
 from Visualization import visualizeImages
 
+ageRange = [10, 100]
 num_ages = 100
 batch_size = 1000
 imageSize = 64
@@ -16,8 +17,11 @@ square_root_fc = int(sqrt(fully_connected_size))
 
 #build the generator network
 dropout = tf.placeholder(tf.float32)
-gen_input_layer = tf.placeholder(tf.float32, shape=[batch_size, input_noise_size])
-gen_fully_connected1, var_dict = create_fully_connected_layer(gen_input_layer, fully_connected_size, input_noise_size,
+gen_input_noise = tf.placeholder(tf.float32, shape=[batch_size, input_noise_size])
+gen_input_age = tf.placeholder(tf.float32, shape=[batch_size, 1])
+gen_input_gender = tf.placeholder(tf.float32, shape=[batch_size, 1])
+gen_input_combined = tf.concat(1, [gen_input_age, gen_input_gender, gen_input_noise])
+gen_fully_connected1, var_dict = create_fully_connected_layer(gen_input_combined, fully_connected_size, input_noise_size+2,
                                                               dropout, trainable=True, name_prefix="gen_fc")
 gen_squared_fc1 = tf.reshape(gen_fully_connected1, [batch_size, square_root_fc, square_root_fc, 1])
 #now [1000,8,8,1]
@@ -56,7 +60,9 @@ loader.start()
 #start training
 while True:
     noise_batch = np.random.random_sample((batch_size, input_noise_size))
-    feed_dict = {gen_input_layer: noise_batch, dropout: 0.5}
+    ageVec = (np.linspace(start=ageRange[0], stop=ageRange[1], num=batch_size) + np.random.sample(batch_size)).reshape([batch_size, 1])
+    genderVec = np.tile(np.array([0, 1], dtype=bool), int(batch_size/2)).reshape([batch_size, 1])
+    feed_dict = {gen_input_noise: noise_batch, gen_input_age: ageVec, gen_input_gender:genderVec, dropout: 0.5}
     print("running")
     generatedImages = sess.run(gen_output_layer, feed_dict=feed_dict)
     generatedImages = np.reshape(generatedImages, [batch_size, imageSize, imageSize, 3])

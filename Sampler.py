a949a60c84244af9cb6e89f85b4f5eb1e0a87012
Daniel Sanche <d.sanche14@gmail.com>
2016-12-01 14:41:24 -0600

    Added function to view the progression of age in a sample

--- Sampler.py
+++ Sampler.py
@@ -23,6 +23,20 @@ def randomSample(network, sampleSize, gender=None, age=None, saveName=None):
         visualizeImages(samples, numRows=numRows, fileName=saveName)
     return samples
 
+def ageSample(network, numImages, minAge=25, maxAge=65, gender=None, noiseArr=None, saveName=None):
+    if gender is None:
+        gender = np.random.randint(2, size=1)
+    if noiseArr is None:
+        noiseArr = np.random.uniform(-1, 1, [1, network.noise_size]).astype(np.float32)
+    ageMat = (((np.linspace(minAge, maxAge, numImages) / 100) * 2) - 1).reshape([numImages, 1])
+    genderMat = ((np.ones([numImages, 1]) * gender) * 2) - 1
+    noiseMat = np.ones([numImages, network.noise_size]) * noiseArr
+    samples = network.getSample(noiseMat, genderMat, ageMat)
+    if saveName is not None:
+        visualizeImages(samples, numRows=1, fileName=saveName)
+    return samples
+
+
 if __name__ == "__main__":
     # initialize the data loader
     saveSteps = 10
@@ -34,4 +48,5 @@ if __name__ == "__main__":
     # start training
     network = NeuralNet(batch_size=batch_size, image_size=image_size, noise_size=noise_size, learningRate=5e-4)
 
-    sample = randomSample(network, 100, saveName="sample.png")
+    randomSample(network, 100, saveName="sample.png")
+    ageSample(network, 20, saveName="age_sample.png")

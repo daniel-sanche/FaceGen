7427c5e19f3513d19b8aa4592ccc9f117b1694d9
Daniel Sanche <d.sanche14@gmail.com>
2016-11-30 22:18:26 -0600

    removed unused code

--- Trainer.py
+++ Trainer.py
@@ -2,30 +2,6 @@ from NeuralNet import  NeuralNet
 from DataLoader import  LoadFilesData, DataLoader
 import numpy as np
 
-def stopFuncGenerator(costDict):
-    truth = costDict["truth"]
-    return truth > 0.7
-
-def stopFuncDiscriminator(costDict):
-    truth =  costDict["truth"]
-    sex = costDict["sex"]
-    return truth > 0.85 and sex > 0.6
-
-
-def trainNetwork(network, batchDict, printInterval=100, rounds=100, endEarlyFunc=None):
-    i = 0
-    endEarly = False
-    while i < rounds and not endEarly:
-        batchImage = batchDict["image"]
-        batchAge = batchDict["age"]
-        batchSex = batchDict["sex"]
-        batchImage = batchImage.reshape([batchImage.shape[0], -1])
-        if i % printInterval == 0:
-            network.printStatus(batchImage, batchSex, batchAge)
-        network.train(batchImage, batchSex, batchAge)
-        i = i + 1
-    network.saveCheckpoint(i)
-
 if __name__ == "__main__":
     # initialize the data loader
     datasetDir = "/home/sanche/Datasets/IMDB-WIKI"

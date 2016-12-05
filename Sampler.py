b8449aa6c0482e687c7d60efc64024fe41b557de
Daniel Sanche <d.sanche14@gmail.com>
2016-12-05 13:22:30 -0600

    fixed bug in age sampler

--- Sampler.py
+++ Sampler.py
@@ -28,7 +28,7 @@ def ageSample(network, numImages, minAge=25, maxAge=65, gender=None, noiseArr=No
         gender = np.random.randint(2, size=1)
     if noiseArr is None:
         noiseArr = np.random.uniform(-1, 1, [1, network.noise_size]).astype(np.float32)
-    ageMat = (((np.linspace(minAge, maxAge, numImages, dtype=int) / 100) * 2) - 1).reshape([numImages, 1])
+    ageMat = (((np.linspace(minAge, maxAge, numImages, dtype=int) / 100.0) * 2) - 1).reshape([numImages, 1])
     genderMat = ((np.ones([numImages, 1]) * gender) * 2) - 1
     noiseMat = np.ones([numImages, network.noise_size]) * noiseArr
     samples = network.getSample(noiseMat, genderMat, ageMat)
@@ -48,5 +48,5 @@ if __name__ == "__main__":
     # start training
     network = NeuralNet(batch_size=batch_size, image_size=image_size, noise_size=noise_size, learningRate=5e-4)
 
-    randomSample(network, 100, saveName="sample.png")
-    ageSample(network, 20, saveName="age_sample.png")
+    randomSample(network, 36, saveName="sample.png")
+    ageSample(network, 10, saveName="age_sample.png")
